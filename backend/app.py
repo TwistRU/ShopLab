2bb37ed481b192e5a38ba866b19ba9a12a491261
Denis <dennis220802@gmail.com>
2021-06-24 23:48:34 +1000

    Добавлен порт

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import send_from_directory, render_template
 
 from backend.config import app, db
@@ -23,4 +25,5 @@ app.register_blueprint(api_v1)
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
