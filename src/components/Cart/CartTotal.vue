47b88b916cea19f5f25da203633e4e036dcc1329
TwistRU <pavlyuk.rs@students.dvfu.ru>
2021-06-27 17:48:51 +1000

    Добавлена кнопка перехода на страницу "оплаты"

---
+++ src/components/Cart/CartTotal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cart-total">
+    <p>ЦЕНА: {{ sum_in_cart }}</p>
+    <button v-if="CART.length"
+            @click="purchaseBtn">
+      Приобрести
+    </button>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "CartTotal",
+  methods: {
+    purchaseBtn() {
+      if(this.IS_AUTHORIZED){
+        this.$router.push({name:'purchase'})
+      }else{
+        this.$router.push({name:'auth'})
+      }
+    },
+  },
+  computed: {
+    ...mapGetters([
+      'CART',
+      'IS_AUTHORIZED',
+    ]),
+    sum_in_cart: function () {
+      let sum = 0;
+      for (let item in this.CART)
+        sum += Number(this.CART[item]['price']) * Number(this.CART[item]['count']);
+      return sum;
+    },
+  },
+}
+</script>
+
+<style scoped lang='scss'>
+.cart-total {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
